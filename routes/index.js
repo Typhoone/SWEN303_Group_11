6fae9fab54bb46494db9ee123ec658b2c0ecdf42
CLTofts <toftsch@gmail.com>
2016-05-21 13:37:29 +1200

    adding part of item page and excluding .idea folder

--- routes/index.js
+++ routes/index.js
@@ -27,5 +27,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
   res.send
 });
+/*
+router.get('/item', function(request, response){
+    pg.connect(DATABASE_URL, function(err, client, done){
+        if(err){
+            console.error(err);
+            response.send("Error " + err);
+        }
+        else{
+            client.query('Select * FROM test_table WHERE name == ' + req.query.name, function(err, result){
+                done();
+                if(err){
+                    console.error(err);
+                    response.send("Error " + err);
+                }
+                else{
+                    responce.render('item', {result:result.rows})
+                }
+            })
+        }
+    })
+})
+*/
 
+router.get('/item', function(req, res, next){
+    res.render('item')
+});
 module.exports = router;

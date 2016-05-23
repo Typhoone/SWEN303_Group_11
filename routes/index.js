444d427fa0c1061c4c9caf3412f2c6979e1a0624
Samual Holmes <baldwisamu1@copperhead.ecs.vuw.ac.nz>
2016-05-23 23:10:35 +1200

    Added 3 most recent items to home page

--- routes/index.js
+++ routes/index.js
@@ -142,7 +142,24 @@ router.get('/search', function(req, res, next) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Trader' });
+  pg.connect(DATABASE_URL, function(err, client, done){
+        if(err){
+            console.error(err);
+            res.send("Error " + err);
+        }
+        else{
+            client.query('SELECT * FROM items ORDER BY id DESC LIMIT 3', function(err, result){
+                done();
+                if(err){
+                    console.error(err);
+                    res.send("Error " + err);
+                }
+                else{
+                    res.render('index', {title: 'Trader', result: result.rows});
+                }
+            });
+        }
+    });
 });
 
 router.get('/browse', function(req, res, next) {
